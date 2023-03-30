41f7019338dec0c772261266c86513db8791b073
Leonid <Chunl3e666@yandex.ru>
2023-03-30 17:47:39 +0300

    заголовок

--- app/tops/head.tsx
+++ app/tops/head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
     return (
       <>
-        <title>Film Assistent</title>
+        <title>Кинотаймер</title>
         <meta content='width=device-width, initial-scale=1' name='viewport' />
-        <meta name='description' content='Топ фильмов по месяцам' />
+        <meta name='description' content='Кинотаймер, Топ фильмов по месяцам' />
         <meta name='keywords' content='Кино, сериалы, помощник' />
         <link rel='icon' href='/favicon.ico' />
       </>
