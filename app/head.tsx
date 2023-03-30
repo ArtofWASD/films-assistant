a302d8299293eeb7676441d5b52d864c43923e81
Leonid <Chunl3e666@yandex.ru>
2023-03-30 14:50:28 +0300

    название и слоган

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title>Film Assistent</title>
+      <title>Кинотаймер</title>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
-      <meta name='description' content='Поможем запомнить что посмотреть' />
+      <meta name='description' content='Ваш личный кинокалендарь!' />
       <meta name='keywords' content='Кино, сериалы, помощник' />
       <link rel='icon' href='/favicon.ico' />
     </>
--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ import MyInput from '../src/utils/ui/input';
 const Page = () => {
   return (
     <main>
-      <div className='flex justify-center py-4 text-xl'>Main Page</div>
+      <div className='flex justify-center py-4 text-xl'>Кинотаймер. Твой личный кинокалендарь!</div>
       <div className='grid  grid-cols-10 h-96 mx-5 gap-2'>
       <LeftMenu />
-      <div className='w-100 h-full bg-slate-400 col-span-9 text-3xl text-center'>основной блок</div>
+      <div className='w-100 h-full bg-slate-200 bg-opacity-25 col-span-9 text-3xl text-center'>основной блок</div>
 
       </div>
 
