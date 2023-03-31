8cb80e4c1cbd47b8a9b3edcaef4d44271c717841
Leonid <Chunl3e666@yandex.ru>
2023-03-31 12:54:08 +0300

    динамические роуты для топов

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,17 @@ import LeftMenu from '../src/components/left-menu/left-menu';
 const Page = () => {
   return (
     <main>
-      <h1 className="text-center py-4">Кинотаймер. Твой личный кинокалендарь!</h1>
-      <div className='grid grid-cols-10 h-96 mx-5 gap-2'>
-      <LeftMenu />
+      <h1 className='text-center py-4'>
+        Кинотаймер. Твой личный кинокалендарь!
+      </h1>
+      <div className='grid grid-cols-10 h-96 mx-5 gap-2 '>
+        <div className='col-start-2'>
+          <LeftMenu />
+        </div>
         <div className='w-100 h-full bg-slate-200 bg-opacity-25 col-start-3 col-span-7 text-3xl text-center'>
           <p className='pt-24'>Main</p>
+        </div>
       </div>
-
-      </div>
-
     </main>
   );
 };
