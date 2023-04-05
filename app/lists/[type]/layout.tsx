c95fe85acc9cb4961168d7444407bcb1ade48458
Leonid <Chunl3e666@yandex.ru>
2023-04-05 17:45:20 +0300

    заголовки для страницы

--- app/lists/[type]/layout.tsx
+++ app/lists/[type]/layout.tsx
@@ -1,13 +1,34 @@
 interface topsLayoutProps {
-    children: React.ReactNode;
-  }
-  
-  const TopsLayout: React.FC<topsLayoutProps> = ({ children }) => {
-    return (
-      <section className="grid justify-items-center">
-        <h1 className="py-4">Новинки за месяц</h1>
-        {children}
-      </section>
-    );
+  children: React.ReactNode;
+  params: {
+    type: string;
   };
-  export default TopsLayout;
+}
+
+const TopsLayout: React.FC<topsLayoutProps> = ({ children, params }) => {
+  let type = '';
+  switch (params.type) {
+    case 'tv_series':
+      type = 'тв сериалов.';
+      break;
+    case 'film':
+      type = 'фильмов.';
+      break;
+    case 'mini_series':
+      type = 'мини сериалов.';
+      break;
+    case 'tv_show':
+      type = 'тв-шоу.';
+      break;
+    default:
+      type = '';
+  }
+
+  return (
+    <section className='grid justify-items-center'>
+      <h1 className='py-4'>Новинки {type}</h1>
+      {children}
+    </section>
+  );
+};
+export default TopsLayout;
