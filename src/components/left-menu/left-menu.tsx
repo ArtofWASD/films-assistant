3f3f38b663a9765b04b475d9db0ed9e59cc9a208
Leonid <Chunl3e666@yandex.ru>
2023-04-14 17:09:51 +0300

    анимации

--- src/components/left-menu/left-menu.tsx
+++ src/components/left-menu/left-menu.tsx
@@ -2,19 +2,37 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
+import { motion } from 'framer-motion';
 
 const LeftMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
-
 
   const handleToggle = () => {
     setIsOpen(!isOpen);
   };
 
+  const variants = {
+    open: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        x: { stiffness: 1000, delay:0.5, velocity:-100 }
+      }
+    },
+    closed: {
+      x:-200,
+      opacity: 0,
+      transition: {
+        x: { stiffness: 1000, delay:0.5 }
+      },
+    }
+  }
   return (
     <>
       <Image
-        src={`${isOpen? '/images/menu-burger-active.png':'/images/menu-burger.png'}`}
+        src={`${
+          isOpen ? '/images/menu-burger-active.png' : '/images/menu-burger.png'
+        }`}
         alt='menu-burger'
         width={0}
         height={0}
@@ -22,45 +40,51 @@ const LeftMenu = () => {
         className='h-10 w-10 ml-3'
         onClick={handleToggle}
       />
-      <div
+      <motion.div
         className={`${
-          isOpen
-            ? 'backdrop-blur-sm absolute h-screen w-screen'
-            : 'hidden'
+          isOpen ? 'backdrop-blur-sm absolute h-screen w-screen bg-gradient-to-r from-slate-300 to bg-transparent mt-2' : 'hidden'
         }`}
         onClick={handleToggle}
+        animate={isOpen ? "open" : "closed"}
+        variants={variants}
       />
-      <ul
-        className={`${
-          isOpen ? 'block mt-3' : 'hidden'
-        } flex flex-col w-full md:w-96 absolute py-4 bg-gradient-to-r from-slate-300 to bg-transparent h-screen gap-2`}
-        onClick={handleToggle}>
-        <li className='font-semibold hover:text-orange-600 text-md hover:drop-shadow-xl text-left pl-1'>
-          <Link href='/lists/film' onClick={handleToggle}>
-            Фильмы
-          </Link>
-        </li>
-        <li className='font-semibold hover:text-orange-600 text-md hover:drop-shadow-xl text-left pl-1'>
-          <Link href='/lists/tv_series' onClick={handleToggle}>
-            Сериалы
-          </Link>
-        </li>
-        <li className='font-semibold hover:text-orange-600 text-md hover:drop-shadow-xl text-left pl-1'>
-          <Link href='/lists/mini_series' onClick={handleToggle}>
-            Мини сериалы
-          </Link>
-        </li>
-        <li className='font-semibold hover:text-orange-600 text-md hover:drop-shadow-xl text-left pl-1'>
-          <Link href='/lists/tv_show' onClick={handleToggle}>
-            Тв сериалы
-          </Link>
-        </li>
-        <li className='font-semibold hover:text-orange-600 text-md hover:drop-shadow-xl text-left pl-1'>
-          <Link href='/' onClick={handleToggle}>
-            На главную
-          </Link>
-        </li>
-      </ul>
+      <motion.div
+      animate={isOpen ? "open" : "closed"}
+      variants={variants}
+      >
+        <ul
+          className={`${
+            isOpen ? 'block mt-2' : 'hidden'
+          } flex flex-col w-full md:w-96 absolute py-4  h-screen gap-2`}
+          onClick={handleToggle}
+        >
+          <li className='font-semibold hover:text-orange-600 text-md hover:drop-shadow-xl text-left pl-1'>
+            <Link href='/lists/film' onClick={handleToggle}>
+              Фильмы
+            </Link>
+          </li>
+          <li className='font-semibold hover:text-orange-600 text-md hover:drop-shadow-xl text-left pl-1'>
+            <Link href='/lists/tv_series' onClick={handleToggle}>
+              Сериалы
+            </Link>
+          </li>
+          <li className='font-semibold hover:text-orange-600 text-md hover:drop-shadow-xl text-left pl-1'>
+            <Link href='/lists/mini_series' onClick={handleToggle}>
+              Мини сериалы
+            </Link>
+          </li>
+          <li className='font-semibold hover:text-orange-600 text-md hover:drop-shadow-xl text-left pl-1'>
+            <Link href='/lists/tv_show' onClick={handleToggle}>
+              Тв сериалы
+            </Link>
+          </li>
+          <li className='font-semibold hover:text-orange-600 text-md hover:drop-shadow-xl text-left pl-1'>
+            <Link href='/' onClick={handleToggle}>
+              На главную
+            </Link>
+          </li>
+        </ul>
+      </motion.div>
     </>
   );
 };
