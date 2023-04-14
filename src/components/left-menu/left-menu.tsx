1cef7f71c756c328792c2afef0895649805aa542
Leonid <Chunl3e666@yandex.ru>
2023-04-14 15:27:39 +0300

    Стилизация бургер меню, без анимации

--- src/components/left-menu/left-menu.tsx
+++ src/components/left-menu/left-menu.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
 
@@ -11,25 +12,47 @@ const LeftMenu = () => {
 
   return (
     <>
-      <button
-        className='block text-white rounded-md relative pl-3 font-semibold'
+      <Image
+        src={`${isOpen? '/images/menu-burger-active.png':'/images/menu-burger.png'}`}
+        alt='menu-burger'
+        width={0}
+        height={0}
+        sizes='100vw'
+        className='h-10 w-10 ml-3'
         onClick={handleToggle}
+      />
+      <div
+        className={`${
+          isOpen
+            ? 'backdrop-blur-sm absolute h-screen w-screen'
+            : 'hidden'
+        }`}
+        onClick={handleToggle}
+      />
+      <ul
+        className={`${
+          isOpen ? 'block mt-3' : 'hidden'
+        } flex flex-col w-96 absolute py-4 bg-gradient-to-r from-slate-300 to bg-transparent h-screen`}
       >
-        Меню
-      </button>
-      <div className={`${isOpen ? 'backdrop-blur-sm absolute h-screen w-screen mt-4 ' : 'hidden'}`} />
-      <ul className={`${isOpen ? 'block mt-4' : 'hidden'} flex flex-col w-48 absolute py-4 bg-slate-100 h-screen rounded-b-lg `}>
         <li className='font-semibold hover:text-orange-600 text-md hover:drop-shadow-xl text-left pl-1'>
-          <Link href='/lists/film' onClick={handleToggle}>Фильмы</Link>
+          <Link href='/lists/film' onClick={handleToggle}>
+            Фильмы
+          </Link>
         </li>
         <li className='font-semibold hover:text-orange-600 text-md hover:drop-shadow-xl text-left pl-1'>
-          <Link href='/lists/tv_series' onClick={handleToggle}>Сериалы</Link>
+          <Link href='/lists/tv_series' onClick={handleToggle}>
+            Сериалы
+          </Link>
         </li>
         <li className='font-semibold hover:text-orange-600 text-md hover:drop-shadow-xl text-left pl-1'>
-          <Link href='/lists/mini_series' onClick={handleToggle}>Мини сериалы</Link>
+          <Link href='/lists/mini_series' onClick={handleToggle}>
+            Мини сериалы
+          </Link>
         </li>
         <li className='font-semibold hover:text-orange-600 text-md hover:drop-shadow-xl text-left pl-1'>
-          <Link href='/lists/tv_show' onClick={handleToggle}>Тв сериалы</Link>
+          <Link href='/lists/tv_show' onClick={handleToggle}>
+            Тв сериалы
+          </Link>
         </li>
       </ul>
     </>
