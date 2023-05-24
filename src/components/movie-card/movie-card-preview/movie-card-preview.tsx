fdc998123a66db193497d05f6ef93ce67ab2db4d
Leonid <chunlee666@gmail.com>
2023-05-24 23:02:58 +0300

    Разибраемся с анимацией

--- src/components/movie-card/movie-card-preview/movie-card-preview.tsx
+++ src/components/movie-card/movie-card-preview/movie-card-preview.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { motion } from 'framer-motion';
 import Image from 'next/image';
 import Genres from '../movie-card-utils/genres/genres';
 import Rating from '../movie-card-utils/rating/rating';
@@ -16,22 +18,50 @@ interface MovieCardPreviewProps {
   };
 }
 const MovieCardPreview: React.FC<MovieCardPreviewProps> = ({ props }) => {
+  const cardMotion = {
+    rest: {
+      x: 0,
+      transition: {
+        type: "tween",
+        ease: "easeIn"
+      }
+    },
+    hover: {
+      transition: {
+        duration: 0.4,
+        type: "tween",
+        ease: "easeOut"
+      }
+    }
+  };
+  const descriptionMotion = {
+    rest: { opacity: 0, ease: "easeOut", duration: 0.4, type: "tween" },
+    hover: {
+      opacity: 1,
+      x:10,
+      transition: {
+        duration: 0.4,
+        type: "tween",
+        ease: "easeIn"
+      }
+    }
+  };
   return (
     <>
       <section className='movie-card rounded-xl p-2 hover:shadow-xl'>
-        <div className='grid grid-flow-col justify-start gap-4'>
-          <div>
+        <motion.div className='grid grid-flow-col justify-start gap-4' initial="rest" whileHover="hover" animate="rest">
+          <motion.div variants={cardMotion}>
             <Image
               src={props.posterUrl}
               alt={props.nameRu? props.nameRu:props.nameEn}
               width={0}
               height={0}
               sizes="100vw"
-              className='md:h-40 md:w-28 h-24 w-16'
+              className='md:h-72 md:w-48 h-24 w-16 '
             />
-          </div>
-          <div>
-            <h2 className="text-sm md:text-base font-semibold whitespace-normal">
+          </motion.div>
+          <motion.div variants={descriptionMotion}>
+            <h2 className="text-md md:text-base font-semibold whitespace-normal">
               {props.nameRu ? <>{props.nameRu}</> : <>{props.nameOriginal}</>}
             </h2>
             <div className='flex gap-1 text-sm md:text-base '>
@@ -61,8 +91,8 @@ const MovieCardPreview: React.FC<MovieCardPreviewProps> = ({ props }) => {
                 <></>
               )}
             </div>
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
       </section>
     </>
   );
