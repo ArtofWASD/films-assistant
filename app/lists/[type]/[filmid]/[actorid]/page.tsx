3fdecaa9a9f8d365243a1ef6686c8450cff1ce29
Art_ofWASD <chunle666@gmail.com>
2023-08-11 20:44:08 +0300

    что то

--- app/lists/[type]/[filmid]/[actorid]/page.tsx
+++ app/lists/[type]/[filmid]/[actorid]/page.tsx
@@ -1,27 +1,23 @@
-import Image from "next/image";
-import getActorInfoApi from "../../../../../src/utils/handlers/getActorInfoApi";
-interface ActorPageProps{
-    params: {
-        actorid: number;
-    }
+import Image from 'next/image';
+import getActorInfoApi from '../../../../../src/utils/handlers/getActorInfoApi';
+interface ActorPageProps {
+  params: {
+    actorid: number;
+  };
 }
 
 const ActorPage = async ({ params }: ActorPageProps) => {
-const data = await getActorInfoApi(params.actorid)
-const actor = data.docs[0]
+  const data = await getActorInfoApi(params.actorid);
+  const actor = data.docs[0];
 
   return (
-    <div>
-        <div className="text-white">ID актёра{params.actorid}</div>
-         <h1>{actor.name}</h1>
-         <Image
-          src={actor.photo}
-          alt={actor.name}
-          width={200}
-          height={300}
-         />
-    </div>
-  )
-}
+    <>
+      <h1 className='py-4 h-8'>{actor.name}</h1>
+      <div className='actor_info grid grid-cols-2'>
+        <Image src={actor.photo} alt={actor.name} width={100} height={150}/>
+      </div>
+    </>
+  );
+};
 
-export default ActorPage
+export default ActorPage;
