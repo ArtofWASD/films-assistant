0b9c8ff90a4e3cac3b6dcd1db8198fd5c2a4d09c
Leonid <Chunl3e666@yandex.ru>
2023-10-20 13:24:31 +0300

    немного вёрстки

--- app/lists/[type]/[filmid]/[actorid]/page.tsx
+++ app/lists/[type]/[filmid]/[actorid]/page.tsx
@@ -5,18 +5,53 @@ interface ActorPageProps {
     actorid: number;
   };
 }
+interface filmInfoProps {
+  id: number;
+  name: string;
+  rating: number;
+  description: string;
+}
 
 const ActorPage = async ({ params }: ActorPageProps) => {
+  // получаем актёра по его ID, так как API отдаёт файл в виде массива, то информацию забираем по первому ключу из массива.
   const data = await getActorInfoApi(params.actorid);
   const actor = data.docs[0];
+  const date = new Date(actor.birthday).toLocaleDateString('ru-ru', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+  console.log(date);
 
   return (
-    <>
-      <h1 className='py-4 h-8'>{actor.name}</h1>
-      <div className='actor_info grid grid-cols-2'>
-        <Image src={actor.photo} alt={actor.name} width={100} height={150}/>
+    <div className="grid grid-cols-6">
+      <h1 className="h-8">{actor.name}</h1>
+      <div className="actor_info ">
+        <div className="actor_photo text-white flex gap-4 ">
+          <Image src={actor.photo} alt={actor.name} width={120} height={150} />
+          <div className="flex flex-col gap-1">
+            <div>Дата рождения: {date}</div>
+            <div>Возраст: {actor.age}</div>
+          </div>
+        </div>
+        {actor && (
+          <div>
+            {actor.movies
+              .filter(
+                (item: filmInfoProps) => item.name !== null && item.description !== null,
+              )
+              .map((item: filmInfoProps) => (
+                <>
+                  <ul className="text-white my-1" key={item.id}>
+                    <li className="text-md">{item.name}</li>
+                    <li className="text-sm text-slate-300">{item.description}</li>
+                  </ul>
+                </>
+              ))}
+          </div>
+        )}
       </div>
-    </>
+    </div>
   );
 };
 
